030078691f27ec4664a1879476b6434145e71311
Sam Jett <samjett247@gmail.com>
2018-10-18 01:20:01 -0500

    Data loader makes hour column and there is a repeatUserBinary column added

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def read_data():
+def read_data(amount_to_cv):
     path_to_dir = 'data/'
     # Read train
     train_data = pd.read_csv(path_to_dir + 'Train.csv', encoding = "ISO-8859-1")
@@ -9,14 +9,29 @@ def read_data():
     # Shuffle the data
     train_data = train_data.sample(frac=1)
     
+    # Convert visitStartTime to hour
+    train['datetime'] = pd.to_datetime(train['visitStartTime'], unit='s')
+    train['hour'] = train['datetime'].dt.hour
+    train.drop(columns = ['datetime', 'visitStartTime', ''], inplace=True)
+    
     # Split the data into CV and train
-    train_split = train_test_split(train_data, test_size=0.3)
+    train_split = train_test_split(train_data, test_size = amount_to_cv)
+    
     train = train_split[0]
     cv= train_split[1]
     
+    # Read the test data
     test_data = pd.read_csv(path_to_dir + 'Test.csv', encoding = "ISO-8859-1")
     
-    return train, cv, test_data
+    # Shuffle the test data
+    test_data = test_data.sample(frac=1)
+    
+    # Convert visitStartTime to hour
+    test_data['datetime'] = pd.to_datetime(test_data['visitStartTime'], unit='s')
+    test_data['hour'] = test_data['datetime'].dt.hour
+    test_data.drop(columns = ['datetime', 'visitStartTime', ''], inplace=True)
+
+    return train, cv, test
 
 def main():
     train, cv, test = read_data()
